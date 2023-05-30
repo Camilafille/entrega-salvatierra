920a77f4d027a31ae5703228b044f6dd8fb0b759
camila salvatierra <salvatierracm@gmail.com>
2023-05-30 20:56:47 -0300

    nuevo commit

--- main.js
+++ main.js
@@ -1,96 +1,41 @@
+//Eventos para el formulario:
+
+let boton = document.getElementById("boton");
+boton.addEventListener("mouseover", () => {
+    boton.className = "enviar" });
+
+boton.addEventListener("mouseout", () => {
+boton.className = "submit"});
 
 
+let mail = document.getElementById("mail");
+mail.addEventListener("input", () => {
+    if(!mail.value.includes("@")){
+        mail.className = "incorrecto"}
+});
 
-// RESERVA
+//Almacen en localStorage de los valores del input
 
-let reserva = "si";
-const bienvenida = prompt("Bienvenido a nuestro Hotel, ¿Desea realizar una reserva?. Si su prespuesta es afirmativa ingrese SI.");
+let formulario = document.getElementById('formulario');
 
-const bienvenidaPagina = () => {
-    let bienvenida = "Bienvenido/a a nuestra pagina web";
-    return alert(bienvenida);
-}
+    formulario.addEventListener("submit", (e) => {
+        e.preventDefault();
+    // Aca vamos a obtener ños valores del los input
+    const inputs = formulario.querySelectorAll(".input");
+    const formDatos = {};
 
-const cantidadPersonas = () => {
-    let cantidadAdultos = parseInt(prompt("Ingrese cantidad de adultos a hospedarse."));
-        let cantidadMenores = parseInt(prompt("Ingrese cantidad de menores a hospedarse."));       
-        let personas = cantidadAdultos + cantidadMenores;
-        return alert(`La cantidad de personas ingresadas son ${personas}.`);
-    }
+    inputs.forEach(input => {
+        formDatos[input.id] = input.value;
+    });
 
-    const calcularDiasHospedaje = (checkIn, checkOut) =>  {
-        const diferencia = new Date(checkOut) - new Date(checkIn);
-        return Math.ceil(diferencia / (1000 * 60 * 60 * 24));
-        }
-        
+    // SE Guarda los valores en localStorage
+    localStorage.setItem("formDatos", JSON.stringify(formDatos));
 
-        const habitaciones = [
-            {id: 1 , nombre: "standar", precio: 30000},
-            {id: 2 , nombre: "superior", precio: 40000},
-            {id: 3 , nombre: "suite", precio: 48000},
-            ];
-        
-
-while(bienvenida.toLowerCase() === "si"){
-    
-        usuario = prompt("Ingrese su nombre y apellido");
-        let mensaje = `Hola ${usuario} bienvenido a nuestro sistema de información. Ingrese los datos requeridos y nos contactaremos a la brevedad.`;
-        alert(mensaje);        
-        cantidadPersonas();
-        const checkIn = prompt("Ingrese la fecha de check-in (yyyy-mm-dd):");
-        const checkOut = prompt("Ingrese la fecha de check-out (yyyy-mm-dd):");
-        alert(`Check in: ${checkIn}, Check out: ${checkOut}`);
-        const numDias = calcularDiasHospedaje(checkIn, checkOut);
-    
-const seleccionHabitacion = prompt("Para consultar el precio por noche de las habitaciones: Standar, Superior o Suite. Ingrese nombre de la habitación");
-const habitacionSeleccionada = habitaciones.find((habitacion) => habitacion.nombre.includes(seleccionHabitacion));
-            const iva = x => x * 0.21;
-            const precioPorDia = habitacionSeleccionada.precio + iva(habitacionSeleccionada.precio);
-            const precioTotal = precioPorDia * numDias;
-            alert(`El precio total por ${numDias} día(s) en la habitación ${seleccionHabitacion} es: $${precioTotal}`);
-        
-            let mail = prompt("Ingrese mail para ser contactado por nuestro staff"); 
-            alert(`El mail ingresado es ${mail}` );
-            let respuesta = prompt("¿La información ingresada es correcta?. De ser afirmativo ingrese SI. ");
-
-//Condicional: Si su informacion esta correcta, entonces le damos las gracias y si no lo esta, el ciclo vuelve a empezar.
-        if(respuesta.toLowerCase() === "si"){
-        alert( "Gracias por contactarnos! Nos comunicaremos a la brevedad via mail.");
-        } 
-        break;
-    }
-
-bienvenidaPagina();
+    // Aca se recetea el formulario
+    formulario.reset();
+    });
 
 
-
-
-
-
-// Funcion para saber en que habitacion esta un huesped:
-
-const huespedes = [
-    { nombre: "Luis Rodriguez" , habitacion: 101 },
-    { nombre: "Mary Campos" , habitacion: 102 },
-    { nombre: "Camila Salas" , habitacion: 103 },
-    { nombre: "Franco Polo" , habitacion: 105 },
-    { nombre: "Luis Santorini" , habitacion: 107 },
-    { nombre: "Melisa Camaño" , habitacion: 109 },
-    { nombre: "Valeria Estévez" , habitacion: 104 },
-    { nombre: "Hernan Pérez", habitacion: 106 },
-    ];
-
-
-    let nombreHuesped = (prompt("Ingrese el nombre del huesped"));
-    const encontrado = huespedes.filter(item => item.nombre === nombreHuesped); 
-    encontrado.forEach(item => {
-        let mensaje = `
-            Nombre: ${item.nombre}
-            Habitacion: ${item.habitacion}
-            `;
-        
-            alert(mensaje);
-            });
 
 
 
