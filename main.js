668d09779aed7aa65758f60bd34746ffc845fc04
camila salvatierra <salvatierracm@gmail.com>
2023-05-30 22:22:13 -0300

    nuevo commit

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ boton.addEventListener("mouseover", () => {
 boton.addEventListener("mouseout", () => {
 boton.className = "submit"});
 
+let botonR = document.getElementById("botonR") 
+botonR.addEventListener("click" , () => {
+    localStorage.clear();
+    location.reload();
+} )
 
 let mail = document.getElementById("mail");
 mail.addEventListener("input", () => {
@@ -21,17 +26,13 @@ let formulario = document.getElementById('formulario');
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
     // Aca vamos a obtener ños valores del los input
-    const inputs = formulario.querySelectorAll(".input");
-    const formDatos = {};
-
+    let inputs = formulario.querySelectorAll(".input");
+    let formDatos = {};
     inputs.forEach(input => {
         formDatos[input.id] = input.value;
     });
-
     // SE Guarda los valores en localStorage
     localStorage.setItem("formDatos", JSON.stringify(formDatos));
-
-    // Aca se recetea el formulario
     formulario.reset();
     });
 
